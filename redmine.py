cc918817a3cdceebb8d35096430ad3d3615af18d
Xhunter <kushwah.kartikey14@upesalumni.upes.ac.in>
2023-03-16 09:13:35 +0530

    update(spent_time): updated spent time from week to day

--- redmine.py
+++ redmine.py
@@ -14,8 +14,6 @@ def get_user_data(user_id):
     redmine = Redmine('https://kore.koders.in', key=os.getenv('API_KEY'))
 
     user = redmine.user.get(user_id)
-    for x in user:
-        print(x)
     position = (user['custom_fields'][1]['value'])
     issues = redmine.issue.all(limit=100, assigned_to_id=user_id)
     opened_issues, total_issues = 0, 0
@@ -24,15 +22,11 @@ def get_user_data(user_id):
             opened_issues += 1
         total_issues += 1
 
-    week = []
-    for i in range(0, 7):
-        week.append(((datetime.datetime.now() - datetime.timedelta(days=i)).date()))
-
     hours = 0
-    for day in week:
-        time_entries = redmine.time_entry.filter(user_id=user_id, spent_on=day)
-        for x in time_entries:
-            hours += x.hours
+    time_entries = redmine.time_entry.filter(user_id=user_id, spent_on=datetime.datetime.now().date())
+    for x in time_entries:
+        hours += x.hours
 
     profile_picture = get_profile_picture(user.mail)
     return str(user['firstname'] + " "+ user['lastname']), position, opened_issues, total_issues, hours, profile_picture
+
